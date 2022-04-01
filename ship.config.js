ffc98ca4d2fd192a3896c124c0814527f8633a64
Maria <heymarialungu@gmail.com>
2022-04-01 22:06:28 +0300

    chore(core): add chipjs config

---
+++ ship.config.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    monorepo: {
+        mainVersionFile: 'package.json', // or `lerna.json`, or whatever a json file you can read the latest `version` from.
+        packagesToBump: ['packages/*'],
+        packagesToPublish: ['packages/*'],
+    },
+    versionUpdated: ({ version, releaseType, dir, exec }) => {
+        // update `lerna.json`
+        const lernaConfigPath = path.resolve(dir, 'lerna.json');
+        const lernaConfig = JSON.parse(fs.readFileSync(lernaConfigPath).toString());
+        lernaConfig.version = version;
+        fs.writeFileSync(lernaConfigPath, JSON.stringify(lernaConfig, null, 2));
+
+        // update `src/lib/version.js`
+        const versionPath = path.resolve(dir, 'src/lib/version.js');
+        fs.writeFileSync(versionPath, `export default "${version}";\n`);
+
+        // update dependencies (if you're using yarn workspace)
+        exec(`yarn workspace example-foo add my-lib@${version}`);
+        exec(`yarn workspace example-bar add my-lib@${version}`);
+    },
+};
