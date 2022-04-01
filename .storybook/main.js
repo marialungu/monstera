309751dc2a7dfe7ca9aca0b36e88df35c2f48e9b
Maria <heymarialungu@gmail.com>
2022-04-01 22:56:06 +0300

    chore(core): prepare npm publish

--- .storybook/main.js
+++ .storybook/main.js
@@ -26,6 +26,8 @@ module.exports = {
         },
       ],
     });
+    // This is where we change the order of resolution of main fields
+    config.resolve.mainFields = ['src', 'module', 'main'];
 
     // Return the altered config
     return config;
